589cece9cd5af3e852c0a804b5f01d1ab4874b33
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 17:24:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            double soma;
+
+            Console.Write("Coloque o valor em °c: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            soma = ((n1 / 5) * 9) + 32;
+
+            Console.WriteLine("o valor em °f é = {0}", soma);
+
+        }
+    }
+}
